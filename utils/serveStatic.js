0233b4687f440c2ed5a1c0eba03a05a6c0637e4e
gtr547 <gitarthapathak360@gmail.com>
2026-05-31 04:42:18 +0530

    Enoent/other error handled

--- utils/serveStatic.js
+++ utils/serveStatic.js
@@ -2,6 +2,8 @@ import path from "node:path";
 import fs from "node:fs/promises";
 import { sendResponse } from "./sendResponse.js";
 import { getContentType } from "./getContentType.js";
+import { ifError } from "node:assert";
+
  
  export async function serveStatic(req, res, baseDir){
 
@@ -9,18 +11,25 @@ import { getContentType } from "./getContentType.js";
 
     const filePath = path.join(
         publicDirPath,
-        req.url ==="/" ? "index.html" : req.url
+        req.url === "/" ? "index.html" : req.url
     );
 
     const ext = path.extname(filePath);
 
     const contentType = getContentType(ext);
+
     
     try {
         const content = await fs.readFile(filePath);
         sendResponse(res, 200, contentType, content);
     } catch (error) {
-        console.log(error);
+        if (error.code === "ENOENT") {
+            const content = await fs.readFile(path.join(publicDirPath, "404.html"));
+            sendResponse(res, 404, "text/html", content);
+        }
+        else {
+            sendResponse(res, 500, "text/html", `<html><h1>Error : ${error.code}</h1></html>`);
+        }
     }
 
  }
