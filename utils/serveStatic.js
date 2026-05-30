320b36ff75952844ece56d15885429af1707b9b3
gtr547 <gitarthapathak360@gmail.com>
2026-05-31 03:42:10 +0530

    Frontend served!

--- utils/serveStatic.js
+++ utils/serveStatic.js
@@ -1,16 +1,26 @@
 import path from "node:path";
 import fs from "node:fs/promises";
 import { sendResponse } from "./sendResponse.js";
+import { getContentType } from "./getContentType.js";
  
  export async function serveStatic(req, res, baseDir){
-    const filePath = path.join(baseDir, "Public", "index.html");
+
+    const publicDirPath = path.join(baseDir, "Public");
+
+    const filePath = path.join(
+        publicDirPath,
+        req.url ==="/" ? "index.html" : req.url
+    );
+
+    const ext = path.extname(filePath);
+
+    const contentType = getContentType(ext);
     
     try {
         const content = await fs.readFile(filePath);
-        sendResponse(res, 200, "text/html", content);
+        sendResponse(res, 200, contentType, content);
     } catch (error) {
         console.log(error);
     }
-
 
  }
