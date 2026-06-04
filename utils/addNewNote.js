2fcec6170dd29159a7a15041a6fe762ecf8c195a
gtr547 <gitarthapathak360@gmail.com>
2026-06-05 00:22:14 +0530

    Universal Unique ID(UUID) added to the input data.

--- utils/addNewNote.js
+++ utils/addNewNote.js
@@ -1,14 +1,17 @@
 import fs from "node:fs/promises";
 import path from "node:path";
 import { getData } from "./getData.js";
+import { v4 } from "uuid";
 
 
 
 export async function addNewNote(newNote){
 
     try {
+        const uuid = v4();
+        const updatedNote = {...newNote, uuid: uuid};
         const note = await getData();
-        note.push(newNote);
+        note.push(updatedNote);
         const dataPath = path.join("Data", "data.json");
         await fs.writeFile(
             dataPath,
